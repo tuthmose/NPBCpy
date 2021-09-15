d794af8c2e3cf8226aa203dae16df145fefc24ee
tuthmose <giordano.mancini@gmail.com>
2021-09-15 17:28:43 +0200

    fixes to density_layer

--- density_layer.py
+++ density_layer.py
@@ -1,11 +1,12 @@
 # G Mancini Jul 2021
 
-from collections import defaultdict
 from sys import argv, exit
+import argparse as argp
 import numpy as np
 import scipy as sp
 
-import myparse
+import npbc_io
+import npbc_analysis
 
 Parse = argp.ArgumentParser(description='Calculate density mean and variance  for a molecular group \
                     in concentric layers of constant volume or radius')
@@ -16,14 +17,14 @@ Parse.add_argument("-I","--index",dest="index",default=False,action="store",
     help="GROMACS index file to use with --molecules")
 Parse.add_argument('-b','--begin',help='first frame to use',default=-1,type=int)
 Parse.add_argument('-e','--end',help='last frame to use',default=-1,type=int)
-Parse.add_argument("-m","--molecules",default=False,action="store",\
-Parse.add_argument("-N","--natoms",default=False,action="store",help="number of atoms
-    in select molecules")
-help="select target molecules; use group numbers")
+Parse.add_argument("-m","--molecules",dest="select",default=False,action="store",\
+    help="select target molecules; use group numbers")
+Parse.add_argument("-N","--natoms",default=False,action="store",help="number of atoms \
+    in selected molecules")
 Parse.add_argument("-R","--rsphere",default=False,action="store",\
     help="radius for spherical boxes (angs)")
-Parse.add_argument("-n","--nbins",action="store",type=int,default=500,\
-    help="number of bins to use; default=500")
+Parse.add_argument("-n","--nbins",action="store",type=int,default=10,\
+    help="number of bins to use; default=10")
 Parse.add_argument("-V","--volume",default=False,action="store_true",\
     help="Use constant volume layers instead of constant radius")
 Parse.add_argument("-w","--from_wall",default=False,action="store_true",\
@@ -62,11 +63,16 @@ if not Myarg.select:
 else: 
     select = list(map(int, Myarg.select))
 
-if not Mayrg.natoms:
+if not Myarg.natoms:
     print("Perception of residues from topology nyi")
     raise ValueError("Missing number of atoms in target molecule type")
 else:
     natoms = int(Myarg.natoms)
+
+if not Myarg.nbins:
+    raise ValueError("missing number of bins")
+else:
+    nbins = int(Myarg.nbins)
     
 # CONSTANTS
 DIM = 3
@@ -75,15 +81,17 @@ nm3_l = 10**(-24)
 csph = (4.0/3.0)*np.pi
     
 # RUN
-target = myparse.parse_index(Myarg.index, select)
+target = npbc_io.parse_index(Myarg.index, select).pop()
 
-traj, first_frame, last_frame = myparse.loadtrj(Myarg.begin, Myarg.end, Myarg.input, top=Myarg.topology)
+traj, first_frame, last_frame = npbc_io.loadtrj(Myarg.begin, Myarg.end, Myarg.input, top=Myarg.topology)
 top = traj.topology
-W = np.array([a.element.mass for a in list(top.atoms)[target]],dtype=np.float32)
+top = traj.topology
+atoms = np.asarray(list(top.atoms))
+W = np.array([a.element.mass for a in atoms[target]],dtype=np.float32)
 nmol = len(target)/natoms
 
-vol,Radii = myparse.gen_bin_radii(target, Nat, nbins, RSphere)
-rho = libl402.calc_density(first_frame, last_frame, vol, traj, target, natoms, nmol, Radii, W)
+vol,Radii = npbc_io.sphere_radii(target, natoms, nbins, RSphere)
+rho = npbc_analysis.calc_density(first_frame, last_frame, vol, traj, target, natoms, nmol, Radii, W)
 np.savetxt(options.outname+".dat",RHO.T,fmt="%15.6f")
         
-quit()        
+quit()
